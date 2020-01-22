e51e5148be1dba8e161a5174a9c807e5665f0b69
Brodie <brodiecarlisle@gmail.com>
2020-01-21 20:21:31 -0700

    updated gitignore

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+//creates a new object
 var g = G$('John', 'Doe');
+//use chainable methods
 g.greet().setLang('es').greet(true).log();
 
+//uses object when login button is clicked
 $('#login').click(function(){
-    var loginGrtr = G$('John', 'Doe');
     
+    //creates new object
+    var loginGrtr = G$('Jane', 'Doe');
+    
+    //hides div after click
     $('#logindiv').hide();
+    
     
     loginGrtr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
 })
